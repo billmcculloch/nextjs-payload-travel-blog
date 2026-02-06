09a74351f5fae4377d2f4141202bd58a5b8a4c0c
billmcculloch <billrmcculloch@gmail.com>
2026-02-06 21:55:19 +0700

    Add post page

--- src/app/(frontend)/[slug]/page.tsx
+++ src/app/(frontend)/[slug]/page.tsx
@@ -1,5 +1,6 @@
 import { RichText } from '@/components/RichText'
 import { getTrip } from '@/lib/get-trip'
+import { format } from 'date-fns'
 import Link from 'next/link'
 import { notFound } from 'next/navigation'
 import React from 'react'
@@ -20,9 +21,9 @@ export default async function TripPage({ params }: Props) {
   }
 
   return (
-    <section className="py-lg">
-      <div className="flex flex-col lg:flex-row gap-10">
-        <div className="space-y-sm basis-1/3">
+    <section>
+      <div className="flex flex-col lg:flex-row gap-10 relative">
+        <div className="space-y-sm basis-1/3 lg:sticky lg:top-0 lg:self-start pt-lg">
           <div className="flex items-center gap-sm text-sm">
             <Link href="/" className="text-ink underline hover:text-ash">
               Home
@@ -37,9 +38,17 @@ export default async function TripPage({ params }: Props) {
               {trip.type}.
             </h1>
           </div>
+
+          <div className="max-w-prose">
+            <p className="text-hero tracking-tight font-light">
+              {format(trip.visitedAt, 'MMMM yyyy')}
+            </p>
+          </div>
         </div>
 
-        <article className="basis-2/3">
+        <article className="basis-2/3 py-sm lg:py-lg">
+          {' '}
+          {/* Bottom padding on article instead */}
           {trip.content ? <RichText data={trip.content} /> : null}
         </article>
       </div>
