e86cce5751b7ae3dd255f4ade90834fcea40a717
billmcculloch <billrmcculloch@gmail.com>
2026-05-17 22:06:59 +0100

    update middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,23 +4,22 @@ const ROOT_DOMAIN = process.env.NEXT_PUBLIC_ROOT_DOMAIN!
 
 export function middleware(req: NextRequest) {
   const host = req.headers.get('host') || ''
-
   const { pathname } = req.nextUrl
 
-  const isInternalRoute =
-    pathname.startsWith('/api') || pathname.startsWith('/admin') || pathname.startsWith('/_next')
-
-  let tenant = ''
-
-  // At the root, so redirect to the signup page if its not an internal route (/admin or /api)
-  if (host.split('.').length === 1 && !isInternalRoute) {
+  if ((host === 'www' || host === ROOT_DOMAIN) && pathname === '/') {
     return NextResponse.rewrite(new URL('/signup', req.url))
   }
+
+  let tenant = ''
 
   if (host.endsWith(ROOT_DOMAIN)) {
     tenant = host.replace(`.${ROOT_DOMAIN}`, '')
   } else if (host.includes('localhost')) {
     tenant = host.split('.')[0]
+  }
+
+  if (tenant === '') {
+    return NextResponse.next()
   }
 
   const requestHeaders = new Headers(req.headers)
